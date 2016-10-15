9c47f520a77942521ece7d477740ecdbe9ca628a
SkyItsMe <skyd0thabeast@gmail.com>
2016-10-15 11:10:27 -0300

    Update PasswordVerifier.java

--- src/Main/PasswordVerifier.java
+++ src/Main/PasswordVerifier.java
@@ -3,9 +3,30 @@ package Main;
 import AdditionalFeatures.*;	//Con esto podes usar clases de AdditionalFeatures (StringTools puede servir)
 
 public class PasswordVerifier {
-	static public boolean verify (String password) {	//Esto fue los que dijimos en clase, lo del mÈtodo static bla bla bla...
-		//Ac· irÌa el cÛdigo
-		
-		return true;	//Cambi· true por lo que sea
+	static public boolean verify (String str) {//Esto fue los que dijimos en clase, lo del m√©todo static bla bla bla...
+		boolean passcorrect = true;							
+		char[] charArray = str.toCharArray();
+		boolean vocalVerifier = false;
+		char lastChar;
+		int contVocals = 0;
+		int contCons = 0;
+		if(str.length() > 20 || str.length() < 1 ||
+				!StringTools.isInAlphabet(charArray) || !StringTools.isLowerCase(charArray)) passcorrect = false;
+		if (passcorrect){
+			for(int i = 0; i < str.length() ; i++){
+				if (!passcorrect) break;
+				vocalVerifier |= StringTools.isVocal(charArray[i]);
+				if(StringTools.isVocal(charArray[i])){
+					contCons = 0;
+					contVocals++;
+				} else {
+					contVocals = 0;
+					contCons++;
+				}
+				if(contVocals > 2 || contCons > 2) passcorrect = false;
+				//Ac√° faltar√≠a el lastChar para que checkee las dos ultimas letras.
+			}
+		}	
+		return passcorrect;	//Cambi√° true por lo que sea
 	}
 }
