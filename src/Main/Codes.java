d9b15e82e976906f74d3c7d2f02fdaf00f1e3fb8
SkyItsMe <skyd0thabeast@gmail.com>
2016-10-22 10:39:17 -0300

    Update Codes.java

--- src/Main/Codes.java
+++ src/Main/Codes.java
@@ -3,4 +3,5 @@ package Main;
 public class Codes {
 	static String ERROR = "01000101 01010010 01010010 01001111 01010010"; //ERROR en binario
 	static String EOL = "01000101 01001111 01001100"; //EOL en binario (End Of List)
+	static char EMPTYCHAR = "0";
 }
