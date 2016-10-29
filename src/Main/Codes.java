8ba4699327bf162b47afe39f94869c677cb74891
De4dSTyx <De4dSTyx@users.noreply.github.com>
2016-10-29 10:11:13 -0300

    Update Codes.java

--- src/Main/Codes.java
+++ src/Main/Codes.java
@@ -2,6 +2,6 @@ package Main;
 
 public class Codes {
 	static String ERROR = "01000101 01010010 01010010 01001111 01010010"; //ERROR en binario
-	static String EOL = "01000101 01001111 01001100"; //EOL en binario (End Of List)
+	static String EOL = "01000101 01001111 01001100"; //EOL en binario (End Of List) usado para marcar el fin de la lista de contraseñas (PasswordList)
 	static char EMPTYCHAR = "0";
 }
