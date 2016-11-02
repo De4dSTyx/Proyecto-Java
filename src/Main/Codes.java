ed9918a0b6a0d9646cc53fc8c83c183bd7119ce3
De4dSTyx <De4dSTyx@users.noreply.github.com>
2016-11-02 20:24:36 -0300

    41c3b1616469646f20756e2045617374657220456767
    4573746f79207265616c6d656e746520616275727269646f205e5e

--- src/Main/Codes.java
+++ src/Main/Codes.java
@@ -1,8 +1,9 @@
 package Main;
 
 public class Codes {
-	static public String ERROR = "01000101 01010010 01010010 01001111 01010010"; //ERROR en binario
-	static public String EOL = "01000101 01001111 01001100"; //EOL en binario (End Of List) usado para marcar el fin de la lista de contraseñas (PasswordList)
-	static public char EMPTYCHAR = '0';
-	static public char TAB = 9;
+	final static public String ERROR = "01000101 01010010 01010010 01001111 01010010"; //ERROR en binario
+	final static public String EOL = "01000101 01001111 01001100"; //EOL en binario (End Of List) usado para marcar el fin de la lista de contraseñas (PasswordList)
+	final static public char EMPTYCHAR = '0';
+	final static public char TAB = 9;
+	final static public int _45617374657220456767 = -1;
 }
