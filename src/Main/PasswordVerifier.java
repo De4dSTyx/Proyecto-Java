d8a109de929329c440ef1555c0d43c5e18310e86
SkyItsMe <skyd0thabeast@gmail.com>
2016-11-04 21:21:59 -0300

    Mas comentarios...
    :,C

--- src/Main/PasswordVerifier.java
+++ src/Main/PasswordVerifier.java
@@ -1,38 +1,38 @@
 package Main;
 
-import AdditionalFeatures.*;	//Con esto podes usar clases de AdditionalFeatures (StringTools puede servir)
+import AdditionalFeatures.*;	//Import nos permite usar otras clases del otro package, en este caso utilizaremos StringTools
 
-public class PasswordVerifier {
-	static public boolean verify (String str) {//Esto fue los que dijimos en clase, lo del método static bla bla bla...
-		boolean passcorrect = true;		//Variable auxiliar booleana					
+public class PasswordVerifier { //
+	static public boolean verify (String str) {//
+		boolean passcorrect = true; //Variable auxiliar booleana, que indica si la contraseña está bien					
 		char[] charArray = str.toCharArray(); //Creamos un char array y pasamos el string (str) a char array
-		boolean vocalVerifier = false; // Creamos otra boolean específca para ver si la regla de vocales es cierta
-		char lastChar = Codes.EMPTYCHAR;
-		int contVocals = 0;
+		boolean vocalVerifier = false; // Creamos otra variable booleana específca para ver si la regla de vocales es cierta
+		char lastChar = Codes.EMPTYCHAR; //Le damos un valor a la variable lastChar que equivale a '0', usando la constante Codes.EMPTYCHAR
+		int contVocals = 0;  //Declaramos valores para los contadores.
 		int contCons = 0;
 		if(str.length() > 20 || str.length() < 1 ||
-			!StringTools.isInAlphabet(charArray) || !StringTools.isLowerCase(charArray)) passcorrect = false; //Utilizamos todo en opuesto para
-		if (passcorrect){
-			for(int i = 0; i < str.length() ; i++){ //Verificamos cada caracter de la contraseña y pasa lo siguiente
-				if (!passcorrect) break; //Para que no siga desde un principio.
-				vocalVerifier |= StringTools.isVocal(charArray[i]);  //Si vocalVerifier es igual...
-				if(StringTools.isVocal(charArray[i])){ //Contador de Vocales
+			!StringTools.isInAlphabet(charArray) || !StringTools.isLowerCase(charArray)) passcorrect = false; //Si la contraseña no se encuentra en el rango 1 - 20, o tiene caracteres que no estan en el alfabeto, o tiene caracteres en minúscula, passcorrect pasa a ser false porque por lo menos una regla no se cumple
+		if (passcorrect){ //Si con el if anterior passcorrect es false, no hay necesidad de verificar las otras reglas porque la contraseña ya esta mal
+			for(int i = 0; i < str.length() ; i++){ //Verificamos cada caracter de la contraseña
+				if (!passcorrect) break; //Para que no siga desde un principio si ya sabemos que esta mal.
+				vocalVerifier |= StringTools.isVocal(charArray[i]);  //vocalVerifier ese igual a vocalVerifier OR ... bueno, lo que dice ahí
+				if(StringTools.isVocal(charArray[i])){ //Si charArray[i] es vocal, el contador de vocales suma 1
 					contCons = 0;
 					contVocals++;
-				} else {  //Contador de Consonantes
+				} else {  //Si no, entonces el contador de consonantes suma 1
 					contVocals = 0;
 					contCons++;
 				}
-				if(contVocals > 2 || contCons > 2) passcorrect = false; //Si el contador de vocales o el contador de consonantes es mayor a dos...
-				if (lastChar != Codes.EMPTYCHAR) { //Si lastChar no tiene un valor de 0 sigue 
-					if (lastChar != 'o' && lastChar != 'e') { //Verifica si el caracter anterior no es igual a o y también verifica si no es igual a e
-						if (lastChar == charArray[i]) passcorrect = false; //Si el caracter que está siendo verificado es igual al anterior...
+				if(contVocals > 2 || contCons > 2) passcorrect = false; //Si el contador de vocales es mayor a dos o el contador de consonantes mayor a dos entonces passcorrect pasa a ser false porque una regla no se cumple
+				if (lastChar != Codes.EMPTYCHAR) { //Si lastChar no tiene un valor de '0' (si no es el primer char del array)...
+					if (lastChar != 'o' && lastChar != 'e') { //Verifica si el caracter anterior no es igual a 'o' y también verifica si no es igual a 'e' ya que esta es una excepción de la regla 
+						if (lastChar == charArray[i]) passcorrect = false; //Si el caracter que está siendo verificado es igual al anterior, entonces passcorect pasa a ser false.
 					}
 				}
-				lastChar = charArray[i]; //Después de todo eso si logró pasar, efectivamente lastChar ES igual a charArray[i]
+				lastChar = charArray[i]; //Finalmente, lastChar pasa a ser charArray[i], para la próxima la próxima iteración del bucle
 			}
-			passcorrect &= vocalVerifier;
+			passcorrect &= vocalVerifier; //Asignamos el valor de passcorrect como passcorrect AND vocalVerifier, de esta forma si vocalVerifier era false, al terminar el bucle for, passcorrect pasa a ser false porque una regla no se cumple
 		}	
-		return passcorrect;	//Cambiá true por lo que sea
+		return passcorrect;	//Devuelve passcorrect, si pasó todas las pruebas debería ser true
 	}
 }
